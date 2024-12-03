057fdde76997b163a1aa0ef56faf3471f3a11c46
Alexander <sasha_shatokhin_2004@mail.ru>
2024-12-03 13:32:07 +0300

    Добавлен слайдер работающий, исправлены небольшие косяки. Можно делать адаптив и мелкие доработки по слайдеру и тексту в нем.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,28 @@
-document.getElementById("main-action-button").onclick = function () {
-    document.getElementById("products").scrollIntoView({behavior: "smooth"});
-}
+var slideIndex = 1;
 
-let links = document.querySelectorAll(".menu-item > a");
-for (let i = 0; i < links.length; i++) {
-    links[i].onclick = function() {
-        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
-    }
-}
+function showSlides(n) {
+  slideIndex += n;
+  var i;
+  var slides = document.getElementsByClassName('slides');
+  var dots = document.getElementsByClassName('dot');
 
-let buttons = document.getElementsByClassName("product-button");
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function() {
-        document.getElementById("order").scrollIntoView({behavior: "smooth"});
-    }
+  for (i = 0; i < slides.length; i++) {
+    console.log(slideIndex);
+    var slide = document.getElementById(slides[i].id);
+    const { left } = slide.getBoundingClientRect();
+
+    slide.style.transform =
+      slideIndex > slides.length || slideIndex < 1
+        ? `translateX(${left}px)`
+        : n < 0
+        ? `translateX(calc(${left}px + 900px))`
+        : `translateX(calc(${left}px - 900px))`;
+  }
+  slideIndex = slideIndex > slides.length ? slides.length : slideIndex < 1 ? 1 : slideIndex;
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  dots[slideIndex - 1].className += ' active';
+  console.log(slideIndex);
 }
