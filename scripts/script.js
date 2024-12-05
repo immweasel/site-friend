4f21182a19f8c38e0ca0e5f2d845240429a3b999
Alexander <sasha_shatokhin_2004@mail.ru>
2024-12-05 16:34:50 +0300

    Исправлена анимация слайдера, сделаны мелкие исправления и адаптив под телефоны

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,24 @@
-var slideIndex = 1;
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 32,
+  centeredSlides:true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
 
-function showSlides(n) {
-  slideIndex += n;
-  var i;
-  var slides = document.getElementsByClassName('slides');
-  var dots = document.getElementsByClassName('dot');
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-right',
+    prevEl: '.swiper-left',
+  },
 
-  for (i = 0; i < slides.length; i++) {
-    console.log(slideIndex);
-    var slide = document.getElementById(slides[i].id);
-    const { left } = slide.getBoundingClientRect();
+  breakpoints: {
+    920: {
+  slidesPerView: 1.98,
 
-    slide.style.transform =
-      slideIndex > slides.length || slideIndex < 1
-        ? `translateX(${left}px)`
-        : n < 0
-        ? `translateX(calc(${left}px + 900px))`
-        : `translateX(calc(${left}px - 900px))`;
+    },
+    320: {
+      slidesPerView: 1
+    }
   }
-  slideIndex = slideIndex > slides.length ? slides.length : slideIndex < 1 ? 1 : slideIndex;
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
-  dots[slideIndex - 1].className += ' active';
-  console.log(slideIndex);
-}
+});
